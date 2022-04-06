01c6053cb009e8b2259fee549a503cd6482bf9c7
vdso10 <53046799+vdso10@users.noreply.github.com>
2022-04-05 21:05:22 -0300

    Commit 4 - Criando telas de tarefas

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,9 +1,64 @@
+import Head from 'next/head';
+import { FiPlus, FiCalendar, FiEdit2, FiTrash, FiClock } from 'react-icons/fi';
+import styles from './style.module.scss'
 
 
 export default function Board(){
     return(
-        <main>
-            <h1>Pagina Tarefas</h1>
-        </main>
+        <>
+            <Head>
+                <title>Minhas tarefas - Board</title>
+            </Head>
+            <main className={styles.container}>
+                <form>
+                    <input
+                        type='text'
+                        placeholder='Digite sua tarefa...'
+                    />
+                    <button type='submit'>
+                        <FiPlus size={25} color='#17181f' />
+                    </button>
+                </form>
+
+                <h1>Voce tem 2 tarefas</h1>
+
+                <section>
+                    <article className={styles.taskList}>
+                        <p>Aprenda a Criar Projetos com nextjs e react</p>
+                        <div className={styles.actions}>
+                            <div>
+                                <div>
+                                    <FiCalendar size={20} color='#FFB800' />
+                                    <time>05 de Abril de 2022</time>
+                                </div>
+                                <button>
+                                    <FiEdit2 size={20} color='#FFF'/>
+                                    <span>Editar</span>
+                                </button>
+                            </div>
+
+                            <button>
+                                <FiTrash size={20} color='#FF3636'/>
+                                <span>Excluir</span>
+                            </button>
+                        </div>
+                    </article>
+                </section>
+            </main>
+
+            <div className={styles.vipContainer}>
+                <h3>Obrigado por apoiar esse projeto.</h3>
+                <div>
+                    <FiClock size={28} color='#FFF' />
+                    <time>
+                        Última doação a 3 dias.
+                    </time>
+                </div>
+            </div>
+
+        
+        
+        </>
+        
     )
 }
